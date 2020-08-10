48740ff4fc7ea4b41340352c29e39d3ed451fcbe
Sisamgtm07 <sisamgautam0001@gmail.com>
2020-08-10 15:16:22 +0545

    User Login

---
+++ php/login.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+$con = mysqli_connect('localhost', 'root' , '');
+
+mysqli_select_db($con, 'db_register');
+
+$name = $_POST['username'];
+$pass = $_POST['password'];
+
+$s = "SELECT * FROM register where Username = '$name'";
+
+$result = mysqli_query($con, $s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+    $_SESSION['username'] = $name;
+    header('location:createprofile.php');
+}
+else{
+    header('location:../index.html');
+}
+
+?>
