7016ace8d46aba912a2d9daf0cf45840552b6940
Sisamgtm07 <sisamgautam0001@gmail.com>
2020-08-10 15:16:58 +0545

    User Registration

---
+++ php/registration.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+header('location:../index.html');
+$con = mysqli_connect('localhost', 'root' , '');
+
+mysqli_select_db($con, 'db_register');
+
+$name = $_POST['username'];
+$pass = $_POST['password'];
+
+$s = "SELECT * FROM register where Username = '$name'";
+
+$result = mysqli_query($con, $s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+    echo"Username ALready Taken";
+}
+else{
+    $reg = "INSERT into register(Username, Password) 
+            VALUES ('$name', '$pass')";
+
+    mysqli_query($con, $reg);
+    echo"Registration successful";
+}
+
+?>
